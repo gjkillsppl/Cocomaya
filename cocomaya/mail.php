b0d7b30c3d81bc8fc57e3d72baca785204d27c39
gjkillsppl <amin@belerofon.com.mx>
2020-12-12 01:53:31 -0600

    Primer Upload
    primer upload de sitio

---
+++ cocomaya/mail.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent= "From: $name ";
+$recipient = "oddfromeveryangle@gmail.com";
+$subject = "Add Your Subject Here";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+header("location: thank-you.html")
+?>
